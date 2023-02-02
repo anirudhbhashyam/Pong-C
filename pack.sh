4597625c7737ab8b78171aab7a366f6bc5dd6629
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-02-02 04:45:28 +0100

    Fixed compilation steps.

--- pack.sh
+++ pack.sh
@@ -1,8 +1,11 @@
+#!/bin/bash 
+
 set -xe
 
 CC=gcc-12
 FILE_EXTENSION=c
 CFLAGS="-Wall -Wextra -std=c11 -pedantic"
+PKGS="sdl2 sdl2_ttf"
 FILENAME=${1%.*}
 
-${CC} ${CFLAGS} -o ${FILENAME} ${1} $(pkg-config --cflags --libs sdl2 sdl2_ttf)
+${CC} ${CFLAGS} $(pkg-config --cflags ${PKGS}) -o ${FILENAME} ${1} $(pkg-config --libs ${PKGS})
