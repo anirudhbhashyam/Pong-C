135e81fa0f455dc3984117e182e9d6ca7c1e0dff
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-02-04 15:06:03 +0100

    Modified compilation for src dir.

--- pack.sh
+++ pack.sh
@@ -6,6 +6,7 @@ CC=gcc-12
 FILE_EXTENSION=c
 CFLAGS="-Wall -Wextra -std=c11 -pedantic"
 PKGS="sdl2 sdl2_ttf"
-FILENAME=${1%.*}
+EXECUTABLE="app"
+SRC=$(find "./src" -type f -name "*.c")
 
-${CC} ${CFLAGS} $(pkg-config --cflags ${PKGS}) -o ${FILENAME} ${1} $(pkg-config --libs ${PKGS})
+${CC} ${CFLAGS} $(pkg-config --cflags ${PKGS}) -o ${EXECUTABLE} ${SRC} $(pkg-config --libs ${PKGS})
