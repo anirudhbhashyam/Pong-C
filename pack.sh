8c8dad668fc8b162ab3872a4dd433027b379203d
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-02-24 14:57:33 +0100

    Updated pack.

--- pack.sh
+++ pack.sh
@@ -5,15 +5,12 @@ set -xe
 CC=gcc-12
 FILE_EXTENSION=c
 CFLAGS="-Wall -Wextra -std=c11 -pedantic"
-PKGS="sdl2"
+PKGS="sdl2 sdl2_ttf"
 EXECUTABLE="app"
 SRC=$(find "./src" -type f -name "*.c")
 
-if [ $(uname) == "Darwin" ]; then
-    PKGS="${PKGS} sdl2_ttf"
-fi
 if [ $(uname) == "Linux" ]; then
     PKGS="${PKGS} sdl2-ttf"
 fi
 
-${CC} ${CFLAGS} $(pkg-config --cflags ${PKGS}) -o ${EXECUTABLE} ${SRC} $(pkg-config --libs ${PKGS})
+${CC} ${CFLAGS} -o ${EXECUTABLE} ${SRC} $(pkg-config --cflags --libs ${PKGS})
