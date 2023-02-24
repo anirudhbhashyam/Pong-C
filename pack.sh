c3e831c69ec51a0fbca818a102787514e53be1a6
bhashyam.anirudh <bhashyam.anirudh@gmail.com>
2023-02-24 14:41:49 +0100

    Updated sdl2 font package name for os.

--- pack.sh
+++ pack.sh
@@ -5,8 +5,15 @@ set -xe
 CC=gcc-12
 FILE_EXTENSION=c
 CFLAGS="-Wall -Wextra -std=c11 -pedantic"
-PKGS="sdl2 sdl2_ttf"
+PKGS="sdl2"
 EXECUTABLE="app"
 SRC=$(find "./src" -type f -name "*.c")
+
+if [ $(uname) == "Darwin" ]; then
+    ${PKGS}+=" sdl2_ttf"
+fi
+if [ $(uname) == "Linux" ]; then
+    ${PKGS}+=" sdl2-ttf"
+fi
 
 ${CC} ${CFLAGS} $(pkg-config --cflags ${PKGS}) -o ${EXECUTABLE} ${SRC} $(pkg-config --libs ${PKGS})
